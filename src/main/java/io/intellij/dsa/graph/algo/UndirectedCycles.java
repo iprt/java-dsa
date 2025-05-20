26c08ba3be89cd1dc9f7c03ba543b8e2f8294f1b
zhenjie zhu <tech@intellij.io>
2025-05-20 21:48:33 +0800

    🚀 Improve cycle detection and enhance printing logic
    Refactored DFS logic for undirected cycle detection to improve clarity and performance. Enhanced cycle printing to include size and formatted vertex details for better readability. Added minor code comments and localization for improved maintainability.

--- src/main/java/io/intellij/dsa/graph/algo/UndirectedCycles.java
+++ src/main/java/io/intellij/dsa/graph/algo/UndirectedCycles.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Rings
@@ -37,19 +38,21 @@ public class UndirectedCycles extends GraphAlgo {
         return result;
     }
 
-    private void dfs(Vertex vertex, Set<String> visited, Set<String> stack, List<Vertex> path, Result result) {
-        // Mark the current node as visited
+    private void dfs(Vertex vertex, Set<String> visited, Set<String> stack, List<Vertex> path, Result record) {
+        // 标记当前节点为已访问
         visited.add(vertex.name());
-        // Add the current node to the stack
+        // 将当前节点添加到递归栈中
         stack.add(vertex.name());
-        // Add the current node to the path
+        // 将当前节点添加到路径中
         path.add(vertex);
 
+        //  1. 如果邻居未被访问，则递归调用 DFS 继续搜索（注意传递了路径的副本)
+        //  2. 如果邻居已被访问且在当前递归栈中，说明找到了一个环。记录从邻居到当前顶点的路径作为环
         for (Edge edge : this.graph.adjacentEdges(vertex.name())) {
             Vertex neighbor = edge.getTo();
             if (!visited.contains(neighbor.name())) {
                 // Continue the DFS search
-                dfs(neighbor, visited, stack, new ArrayList<>(path), result);
+                this.dfs(neighbor, visited, stack, new ArrayList<>(path), record);
             } else if (stack.contains(neighbor.name())) {
                 // Found a cycle, the neighbor is already in the current path
                 List<Vertex> cycle = new ArrayList<>();
@@ -57,7 +60,7 @@ public class UndirectedCycles extends GraphAlgo {
                 for (int i = index; i < path.size(); i++) {
                     cycle.add(path.get(i));
                 }
-                result.cycles.add(cycle);
+                record.cycles.add(cycle);
             }
         }
         // Backtrack: remove the current node from the path
@@ -80,7 +83,9 @@ public class UndirectedCycles extends GraphAlgo {
         }
 
         private void printCycle(List<Vertex> cycle) {
-            System.out.println("Printing cycle:");
+            System.out.println("Printing Cycle|size = " + cycle.size() + "|vertexes = " + cycle.stream().map(Vertex::name).collect(Collectors.joining(" ")));
+            String lineStart = " ".repeat(2);
+
             /*
               A - B - C
               |       |
@@ -89,7 +94,7 @@ public class UndirectedCycles extends GraphAlgo {
             // 打印环，如果只有两个节点，则打印成 A - B
             // 如果超过两个节点，打印成三行，打印出一个可以理解的环
             if (cycle.size() == 2) {
-                System.out.println(cycle.get(0).name() + " - " + cycle.get(1).name());
+                System.out.println(lineStart + cycle.get(0).name() + " - " + cycle.get(1).name());
                 System.out.println();
                 return;
             }
@@ -98,10 +103,11 @@ public class UndirectedCycles extends GraphAlgo {
             boolean isEven = (cycle.size() % 2 == 0);
             int mid = isEven ? cycle.size() / 2 : cycle.size() / 2 + 1;
 
+
             // 打印上半部分
             List<Vertex> upper = cycle.subList(0, mid);
             // A - B - C
-            StringBuilder upBuilder = new StringBuilder();
+            StringBuilder upBuilder = new StringBuilder(lineStart);
             for (int i = 0; i < upper.size(); i++) {
                 if (i == upper.size() - 1) {
                     upBuilder.append(upper.get(i).name());
@@ -112,15 +118,15 @@ public class UndirectedCycles extends GraphAlgo {
             System.out.println(upBuilder);
 
             // 打印中间部分
-            String midStart = "|";
+            String midStart = lineStart + "|";
             String midEnd = isEven ? "|" : "/";
             int sub = isEven ? 2 : 3;
-            System.out.println(midStart + " ".repeat(upBuilder.length() - sub) + midEnd);
+            System.out.println(midStart + " ".repeat(upBuilder.length() - lineStart.length() - sub) + midEnd);
 
             // 打印下半部分
             // D - E - F
             List<Vertex> lower = cycle.subList(mid, cycle.size());
-            StringBuilder downBuilder = new StringBuilder();
+            StringBuilder downBuilder = new StringBuilder(lineStart);
             for (int i = lower.size() - 1; i >= 0; i--) {
                 if (i == 0) {
                     downBuilder.append(lower.get(i).name());
